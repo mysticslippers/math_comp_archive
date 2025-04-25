6ed51612f242ec0036abb26d73b36db35dfdd9da
Mikhailov Dmitry <eternal.martyr13579@gmail.com>
2025-04-25 18:37:24 +0300

    add functions

--- Lab2_comp_math/code/nonlinear_equation.py
+++ Lab2_comp_math/code/nonlinear_equation.py
@@ -5,3 +5,22 @@ import matplotlib.pyplot as plt
 variable = sp.symbols('x')
 INPUT_FILE_PATH = "iofiles/input.txt"
 OUTPUT_FILE_PATH = "iofiles/output.txt"
+
+def f1(x):
+    return sp.cos(x) - x
+
+
+def f2(x):
+    return x ** 3 - x - 2
+
+
+def f3(x):
+    return sp.exp(x) - 3 * x ** 2
+
+
+def f4(x):
+    return x ** 2 - 2
+
+
+def f5(x):
+    return sp.log(x + 2) - x
