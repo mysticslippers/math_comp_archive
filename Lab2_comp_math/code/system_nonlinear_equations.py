1ae49a0e827fcaba40ebc4a8ba3bda8ec357e388
Mikhailov Dmitry <eternal.martyr13579@gmail.com>
2025-05-19 12:26:22 +0300

    fix data

--- Lab2_comp_math/code/system_nonlinear_equations.py
+++ Lab2_comp_math/code/system_nonlinear_equations.py
@@ -4,22 +4,22 @@ import numpy as np
 
 def system1(x):
     return np.array([
-        np.sin(x[0]) + x[1] - 1,
-        np.cos(x[1]) + x[0] - 1
+        -0.3 + 0.1 * x[0]**2 + x[0] + 0.2 * x[1]**2,
+        -0.7 + 0.2 * x[1]**2 + x[1] + 0.1 * x[0] * x[1],
     ])
 
 
 def phi1(x):
     return np.array([
-        np.arcsin(1 - x[1]),
-        np.arccos(1 - x[0])
+        0.3 - 0.1 * x[0]**2 - 0.2 * x[1]**2,
+        0.7 - 0.2 * x[0]**2 - 0.1 * x[0] * x[1],
     ])
 
 
-def derivative_system1(x):
+def derivative_phi_system1(x):
     return np.array([
-        [-1 / (np.sqrt(2 * x[1] - x[1]^2)), 0],
-        [0, -1 / np.sqrt(2 - x[0])]
+        [-0.2 * x[0], -0.4 * x[1]],
+        [-0.4 * x[0] - 0.1 * x[1], -0.1 * x[0]],
     ])
 
 
@@ -37,9 +37,9 @@ def phi2(x):
     ])
 
 
-def derivative_system2(x):
+def derivative_phi_system2(x):
     return np.array([
-        [0, -x[0] / (np.sqrt(4 - x[0]**2))],
+        [0, -x[1] / (np.sqrt(4 - x[1]**2))],
         [1, 0]
     ])
 
