1e956b818af9aee65486d7556f7c63643ff19b2f
Mikhailov Dmitry <eternal.martyr13579@gmail.com>
2025-05-23 20:31:22 +0300

    add data

--- Lab3_comp_math/code/main.py
+++ Lab3_comp_math/code/main.py
@@ -1,98 +1,27 @@
-import numpy as np
+# Лабораторная работа №3 (Вариант №8)
+# Михайлов Дмитрий Андреевич
+# Группа P3206
+
+import sympy as sp
+
+INITIAL_N = 4
+variable = sp.symbols('x')
+
+def f1(x):
+    return x**2 - 1
 
 
-class NumericalIntegration:
-
-    def __init__(self, function):
-        self.function = function
-
-    def left_rectangle(self, a, b, n):
-        h = (b - a) / n
-        total_area = 0
-        for i in range(n):
-            total_area += self.function(a + i * h)
-        return total_area * h
-
-    def right_rectangle(self, a, b, n):
-        h = (b - a) / n
-        total_area = 0
-        for i in range(1, n + 1):
-            total_area += self.function(a + i * h)
-        return total_area * h
-
-    def middle_rectangle(self, a, b, n):
-        h = (b - a) / n
-        total_area = 0
-        for i in range(n):
-            total_area += self.function(a + (i + 0.5) * h)
-        return total_area * h
-
-    def trapezoidal(self, a, b, n):
-        h = (b - a) / n
-        total_area = 0.5 * (self.function(a) + self.function(b))
-        for i in range(1, n):
-            total_area += self.function(a + i * h)
-        return total_area * h
-
-    def simpson(self, a, b, n):
-        if n % 2 == 1:
-            n += 1
-        h = (b - a) / n
-        total_area = self.function(a) + self.function(b)
-        for i in range(1, n):
-            if i % 2 == 0:
-                total_area += 2 * self.function(a + i * h)
-            else:
-                total_area += 4 * self.function(a + i * h)
-        return total_area * h / 3
+def f2(x):
+    return sp.sin(x)
 
 
-def runge_error_estimation(val1, val2):
-    return abs(val1 - val2) / 15
+def f3(x):
+    return x**3 - 3 * x**2 + 6 * x - 19
 
 
-def main():
-    print("Выберите функцию для интегрирования:")
-    print("1. f(x) = x^2")
-    print("2. f(x) = sin(x)")
-    print("3. f(x) = e^x")
-    print("4. f(x) = ln(x + 1)")
-    print("5. f(x) = cos(x)")
-
-    choice = int(input("Введите номер функции (1-5): "))
-    if choice == 1:
-        function = lambda x: x ** 2
-    elif choice == 2:
-        function = lambda x: np.sin(x)
-    elif choice == 3:
-        function = lambda x: np.exp(x)
-    elif choice == 4:
-        function = lambda x: np.log(x + 1)
-    elif choice == 5:
-        function = lambda x: np.cos(x)
-    else:
-        print("Некорректный выбор функции.")
-        return
-
-    a = float(input("Введите нижний предел интегрирования: "))
-    b = float(input("Введите верхний предел интегрирования: "))
-    target_error = float(input("Введите требуемую точность: "))
-
-    integration_method = NumericalIntegration(function)
-    n = 4
-    error = float('inf')
-    current_value = 0
-
-    while error > target_error:
-        last_value = current_value
-        current_value = integration_method.simpson(a, b, n)
-        if n > 4:
-            error = runge_error_estimation(last_value, current_value)
-        n *= 2
-
-    print(f"Значение интеграла: {current_value}")
-    print(f"Число разбиения для достижения требуемой точности: {n // 2}")
+def f4(x):
+    return sp.cos(x)
 
 
-if __name__ == "__main__":
-    main()
+def f5(x):
+    return sp.ln(x + 1)
