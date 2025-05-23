ea1008e93b51a00572dd9ce270c702729ca189d3
Mikhailov Dmitry <eternal.martyr13579@gmail.com>
2025-05-23 20:38:07 +0300

    add middle_rectangle_method function

--- Lab3_comp_math/code/main.py
+++ Lab3_comp_math/code/main.py
@@ -162,3 +162,25 @@ def left_rectangle_method(function, initial_approximation, tolerance):
             return current_result, n
         else:
             n *= 2
+
+
+def middle_rectangle_method(function, initial_approximation, tolerance):
+    current_result = 0
+    n = INITIAL_N
+
+    while n <= n * (2 ** 10):
+        previous_result = current_result
+
+        current_result = 0
+        x = initial_approximation[0]
+        h = (initial_approximation[1] - initial_approximation[0]) / n
+
+        for i in range(n):
+            current_result += compute_function_value(function, x + h / 2)
+            x += h
+        current_result *= h
+
+        if check_runge_error_estimation(previous_result, current_result, tolerance, 2):
+            return current_result, n
+        else:
+            n *= 2
