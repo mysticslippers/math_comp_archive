17ce69168bfc0af62aa0fe5fedba50990d2b7de5
Mikhailov Dmitry <eternal.martyr13579@gmail.com>
2025-05-30 12:17:11 +0300

    add plot function

--- Lab4_comp_math/code/main.py
+++ Lab4_comp_math/code/main.py
@@ -65,6 +65,29 @@ def read_input():
             print(f"Произошла ошибка: {exception}. Пожалуйста, попробуйте снова.")
 
 
+def plot(x, y, plot_x, plot_ys, labels):
+    plt.figure()
+    plt.title("График")
+
+    ax = plt.gca()
+    ax.spines['left'].set_position('zero')
+    ax.spines['bottom'].set_position('zero')
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+
+    ax.plot(1, 0, marker='>', markersize=5, color='k', transform=ax.get_yaxis_transform(), clip_on=False)
+    ax.plot(0, 1, marker='^', markersize=5, color='k', transform=ax.get_xaxis_transform(), clip_on=False)
+
+    plt.scatter(x, y, marker='o', color='b', label='Данные')
+
+    for plot_y, label in zip(plot_ys, labels):
+        plt.plot(plot_x, plot_y, label=label)
+
+    plt.legend()
+    plt.grid(True)
+    plt.show(block=False)
+
+
 def main():
     print("\t\t\t\t\t\tЛабораторная работа №4")
     
