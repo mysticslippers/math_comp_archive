d00ed8f09ba944425c5e00f3b13c0009226485e5
Mikhailov Dmitry <eternal.martyr13579@gmail.com>
2025-05-30 12:15:15 +0300

    update main.py

--- Lab4_comp_math/code/main.py
+++ Lab4_comp_math/code/main.py
@@ -3,6 +3,16 @@
 # Группа P3206
 
 
+import csv
+import numpy as np
+import sympy as sp
+import matplotlib.pyplot as plt
+
+variable = sp.symbols('x')
+INPUT_FILE_PATH = "iofiles/input.txt"
+OUTPUT_FILE_PATH = "iofiles/output.txt"
+
+
 def main():
     print("\t\t\t\t\t\tЛабораторная работа №4")
     
