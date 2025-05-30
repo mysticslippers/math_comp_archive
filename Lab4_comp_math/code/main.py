e28aaa1978e57d62c13465b41b5cb62d22fdb6b1
Mikhailov Dmitry <eternal.martyr13579@gmail.com>
2025-05-30 12:15:52 +0300

    add read_dots function

--- Lab4_comp_math/code/main.py
+++ Lab4_comp_math/code/main.py
@@ -13,6 +13,27 @@ INPUT_FILE_PATH = "iofiles/input.txt"
 OUTPUT_FILE_PATH = "iofiles/output.txt"
 
 
+def read_dots(prompt="\nВводите координаты через пробел, каждая точка с новой строки.\n"):
+    print(prompt)
+    print("Чтобы закончить, введите 'END'.")
+    dots = []
+    while True:
+        try:
+            current = input("> ").strip()
+            if current == "END":
+                if len(dots) < 2:
+                    raise AttributeError
+                return np.array(dots)
+            current_dot = tuple(map(float, current.split()))
+            if len(current_dot) != 2:
+                raise ValueError
+            dots.append(current_dot)
+        except ValueError:
+            print("Введите точку повторно - координаты некорректны!")
+        except AttributeError:
+            print("Минимальное количество точек - 2!")
+
+
 def main():
     print("\t\t\t\t\t\tЛабораторная работа №4")
     
